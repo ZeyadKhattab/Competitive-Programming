21a916fb8b5994c67c1c03d13933027f7a11bbb3
Zeyad Khattab <41998602+ZeyadKhattab@users.noreply.github.com>
2019-08-13 16:19:49 +0200

    Update Template.java

--- Template.java
+++ Template.java
@@ -51,4 +51,20 @@ public class Template {
 
 	}
 
+	static void sort(int[] a) {
+		shuffle(a);
+		Arrays.sort(a);
+	}
+
+	static void shuffle(int[] a) {
+		int n = a.length;
+		Random rand = new Random();
+		for (int i = 0; i < n; i++) {
+			int tmpIdx = rand.nextInt(n);
+			int tmp = a[i];
+			a[i] = a[tmpIdx];
+			a[tmpIdx] = tmp;
+		}
+	}
+
 }
