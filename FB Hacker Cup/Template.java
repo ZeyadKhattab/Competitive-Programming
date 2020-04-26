0765f807c616f23f1b2bceb083a86a2db93d2e37
Zeyad Khattab <41998602+ZeyadKhattab@users.noreply.github.com>
2020-04-26 16:06:25 +0200

    Create Template.java

---
+++ FB Hacker Cup/Template.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner();
+		PrintWriter out = new PrintWriter(System.out);
+		int tc = sc.nextInt();
+		for (int t = 1; t <= tc; t++) {
+			out.printf("Case #%d: ", t);
+
+		}
+		out.close();
+
+	}
+
+	static class Scanner {
+		BufferedReader br;
+		StringTokenizer st;
+
+		Scanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		Scanner(String fileName) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(fileName));
+		}
+
+		String next() throws IOException {
+			while (st == null || !st.hasMoreTokens())
+				st = new StringTokenizer(br.readLine());
+			return st.nextToken();
+		}
+
+		String nextLine() throws IOException {
+			return br.readLine();
+		}
+
+		int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() throws NumberFormatException, IOException {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() throws NumberFormatException, IOException {
+			return Double.parseDouble(next());
+		}
+
+		boolean ready() throws IOException {
+			return br.ready();
+		}
+
+		int[] nxtArr(int n) throws IOException {
+			int[] ans = new int[n];
+			for (int i = 0; i < n; i++)
+				ans[i] = nextInt();
+			return ans;
+		}
+
+	}
+
+	static void sort(int[] a) {
+		shuffle(a);
+		Arrays.sort(a);
+	}
+
+	static void shuffle(int[] a) {
+		int n = a.length;
+		Random rand = new Random();
+		for (int i = 0; i < n; i++) {
+			int tmpIdx = rand.nextInt(n);
+			int tmp = a[i];
+			a[i] = a[tmpIdx];
+			a[tmpIdx] = tmp;
+		}
+	}
+
+}
